d782bc598dcb14281b479cbbb1109485c24291a7
Andrew Yu <andrewyu.au@gmail.com>
2017-12-12 09:16:53 +1000

    Update pipeline-DEPLOY.sh

--- .bluemix/pipeline-DEPLOY.sh
+++ .bluemix/pipeline-DEPLOY.sh
@@ -11,7 +11,7 @@ echo "Using manifest file: $MANIFEST"
 echo "Using prefix: $PREFIX"
 
 # Create CF services
-cf create-service cloudantNoSQLDB Lite ${PREFIX}insurance-policy-db
+cf create-service cloudantNoSQLDB Lite ${PREFIX}student-experience-db
 # Set app's env vars
 domain=".mybluemix.net"
 case "${REGION_ID}" in
